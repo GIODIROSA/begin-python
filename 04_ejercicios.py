e261b13324c44f2b3631d60fc91f9ed18d2205c4
giovanni di rosa <gdirosa@flagare.cl>
2024-03-31 11:14:53 -0300

    [ADD] ejercicios ppt python inicial

--- 04_ejercicios.py
+++ 04_ejercicios.py
@@ -17,7 +17,7 @@ El cambio obtenido por los valores ingresados en dolares y el valor del dolar ac
 """
 print(resultado_casa_cambio, cantidad_pesos_clp, "CLP")
 
-# Generador de correo electrónico con formato nombre.apellido@correo.cl, solicitando los datos al usuario.
+# 3. Generador de correo electrónico con formato nombre.apellido@correo.cl, solicitando los datos al usuario.
 
 nombre_usuario= input("Ingrese el nombre: ")
 apellido_usuario= input("Ingrese el apellido: ")
@@ -26,9 +26,25 @@ formato_nombre= nombre_usuario + "." + apellido_usuario
 generador_correo= formato_nombre + "@inacap.cl"
 print("Su nuevo correo es: ", generador_correo)
 
-#6. Conversor de longitud (Pies a Metros) solicitando los datos al usuario.
+# 4. Conversor de longitud (Pies a Metros) solicitando los datos al usuario.
 
 cantidad_pies= float(input("Ingrese la cantidad de longitud en pies: "))
 conversor_a_metros= round(cantidad_pies // 3,281)
 print("La longitud en metros son: ", conversor_a_metros)
 
+# 5. Solicitar al usuario un número y un porcentaje, para mostrar por pantalla el número correspondiente al porcentaje del número ingresado.
+
+valor_numero = float(input("Ingrese el valor: "))
+valor_porcentaje = int(input("Ingrese el porcentaje aplicar: "))
+reset_porcentaje = valor_porcentaje / 100
+resultado_porcentaje = round(valor_numero * reset_porcentaje)
+print("El", valor_porcentaje, "% de", valor_numero, "es ",  resultado_porcentaje)
+
+
+# 6. Solicitar al usuario el precio de un producto y el porcentaje de descuento, para mostrar por pantalla el precio a pagar después de aplicar el descuento
+
+precio_producto = float(input("Ingrese el precio del producto: "))
+valor_descuento = float(input("Ingrese el descuento aplicar: "))
+resultado_descuento_aplicado= round(precio_producto * (valor_descuento / 100))
+resultado_final= precio_producto - resultado_descuento_aplicado
+print("El valor del producto con porcentaje aplicado es: ", resultado_final)
