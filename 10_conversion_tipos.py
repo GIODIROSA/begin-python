ebc3417673104eb2d62458d2976124f0f6e70d1a
Giovanni Di Rosa <giodirosaartes@gmail.com>
2024-04-01 23:47:51 -0300

    [ADD] control de flujo

---
+++ 10_conversion_tipos.py
@@ -0,0 +1,14 @@
+# x = input("")
+
+# int
+# str
+# float()
+# bool() puede tener problemas // truthy - falsy
+# falsy string vacios | un objeto none | cero 0
+
+print(bool("0"))
+print(bool(""))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
+
