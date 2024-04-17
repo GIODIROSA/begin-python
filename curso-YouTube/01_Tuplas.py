2674b470e108f12f897ce6daca5fec58e49e3ac4
giovanni di rosa <gdirosa@flagare.cl>
2024-04-17 09:52:20 -0400

    [ADD] se añade ejercicio y lambda y map y filter

---
+++ curso-YouTube/01_Tuplas.py
@@ -0,0 +1,13 @@
+# Es lo mismo mismo mismo mismo que una lista
+# solo tiene una sola condición: NO LA PUEDES MODIFICAR EN LO ABSOLUTO
+
+numeros = (1, 2, 3) + (4, 5, 6)
+
+# print(numeros)
+
+# Transformar -> listas a tuple
+punto = tuple([1,2, 3, 4, 5, 6, 7, 8])
+print(punto)
+
+menosNumeros = numeros[:3]
+print(menosNumeros)
---
+++ curso-YouTube/03_expresiones_lambda.py
@@ -0,0 +1,5 @@
+usuarios = [["Chanchito", 4], ["Felipe", 1], ["Pulga", 5]]
+
+usuarios.sort(key=lambda el:el[1]) 
+
+print(usuarios)
