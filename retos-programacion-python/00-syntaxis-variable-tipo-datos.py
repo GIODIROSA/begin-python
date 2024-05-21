55e4a2f66d92fd9de23dd7c50d9eb1c6d460df99
Giovanni Di Rosa <giodirosaartes@gmail.com>
2024-05-21 12:35:01 -0400

    [ADD] primer reto de logica de programacion

--- retos-programacion-python/00-syntaxis-variable-tipo-datos.py
+++ retos-programacion-python/00-syntaxis-variable-tipo-datos.py
@@ -12,10 +12,10 @@
  *
  * EJERCICIO:
  * - Crea un comentario en el código y coloca la URL del sitio web oficial del
- *   lenguaje de programación que has seleccionado.
+ *   lenguaje de programación que has seleccionado. (check)
  * - Representa las diferentes sintaxis que existen de crear comentarios
- *   en el lenguaje (en una línea, varias...).
- * - Crea una variable (y una constante si el lenguaje lo soporta).
+ *   en el lenguaje (en una línea, varias...). (check)
+ * - Crea una variable (y una constante si el lenguaje lo soporta). (check)
  * - Crea variables representando todos los tipos de datos primitivos
  *   del lenguaje (cadenas de texto, enteros, booleanos...).
  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
@@ -25,4 +25,28 @@
  */
  
 """
+
+# https://www.python.org/
+
+"""
+Otra manera de hacer comentarios compuestos
+"""
+
+texto = "soy un string"
+number = 1987
+float = 23,8
+boolean_true= True
+boolean_false= False
+lista= []
+diccionario= {}
+tuples= ()
+
+url_sitio_oficial = "https://www.python.org/"
+
+#formato simple
+print("¡Hola1 ", url_sitio_oficial)
+
+
+#formato de concatenar string
+print(f"¡Hola2, {url_sitio_oficial}")
 
