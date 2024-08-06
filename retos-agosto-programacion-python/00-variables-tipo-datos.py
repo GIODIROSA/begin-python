5b9ccb4382a10d156612c9a4637402a1bdf5f485
giodirosa <giodirosaartes@gmail.com>
2024-08-05 20:13:21 -0400

    [ADD] se adiciona con repaso de python y sus elementos básicos

---
+++ retos-agosto-programacion-python/00-variables-tipo-datos.py
@@ -0,0 +1,49 @@
+"""
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+"""
+
+url_web_oficial= "https://www.python.org/"
+languague= "Python"
+print(f'{url_web_oficial} sitio oficial del lenguaje: {languague}')
+
+## comentarios en python
+"""
+Nota: para los comentarios 
+más extensos en python
+"""
+
+## Tipos de datos
+
+variable_string= "Variable de string";
+variable_number= 1987
+variable_true= True
+variable_false= False
+variable_lista={'nombre':'Giovanni', 'apellido':'DiRosa', 'direccion':'Calle Orense 1 norte' }
+variable_diccionario=['loquesea1','loquesea2','loquesea3']
+variable_float= 23.6
+
+print("Lenguaje:" + languague);
+
+
