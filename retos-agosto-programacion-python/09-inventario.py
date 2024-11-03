ff57913f7014141384e410574d6562ba00ca6ab4
giodirosa <giodirosaartes@gmail.com>
2024-11-03 12:51:39 -0300

    [ADD] se añade poo sistema de inventario

---
+++ retos-agosto-programacion-python/09-inventario.py
@@ -0,0 +1,27 @@
+
+class Producto:
+    def __init__(self, id, nombre, precio, cantidad):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def __str__(self):
+        return f'{self.nombre} (ID: {self.id}, Precio: {self.precio}, Cantidad: {self.cantidad})'
+
+
+class Inventario:
+    def __init__(self):
+        self.productos= {}
+
+    def agregar_producto(self, producto):
+        if producto.id in self.productos:
+            print("Producto ya existe. Actualizando cantidad. ")
+            self.productos[producto.id].cantidad += producto.cantidad
+        else:
+            self.productos[producto.id]= producto
+
+inventario1= Inventario()
+producto1= Producto(id:1, nombre:"Manzana", precio:0.50, cantidad:100)
+inventario1.agregar_producto(producto1)
+
