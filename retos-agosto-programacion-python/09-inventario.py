ae22e487bd3c3bdda47dcbe37818d785fde36234
giodirosa <giodirosaartes@gmail.com>
2024-11-03 15:44:48 -0300

    [ADD] se agrega eliminar producto

--- retos-agosto-programacion-python/09-inventario.py
+++ retos-agosto-programacion-python/09-inventario.py
@@ -14,16 +14,28 @@ class Inventario:
     def __init__(self):
         self.productos= {}
 
+# Agregar productos
     def agregar_producto(self, producto):
         if producto.id in self.productos:
             print("Producto ya existe. Actualizando cantidad. ")
             self.productos[producto.id].cantidad += producto.cantidad
         else:
+            print("Producto ha sido agregado al inventario")
             self.productos[producto.id]= producto
+# ELiminar productos
+    def eliminar_producto(self, id):
+        if id in self.productos:
+            del self.productos[id]
+            print(f'Producto ID: {id}, eliminado con existo!')
+        else:
+            print("Producto no encontrado")
 
 inventario1= Inventario()
 producto1= Producto(id=1, nombre="Manzana", precio=0.50, cantidad=100)
+producto2= Producto(id=2, nombre="Pera", precio=0.75, cantidad=105)
+
 print(inventario1.productos)
 inventario1.agregar_producto(producto1)
+inventario1.agregar_producto(producto2)
 print(inventario1.productos)
 
